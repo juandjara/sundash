df1b00dc5bcb669c88e533c4cbbd880d3de7256a
Juan D. Jara <juanorigami@gmail.com>
2023-11-17 00:22:26 +0100

    add root link

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -6,7 +6,9 @@ export default function Layout({ children }: { children: React.ReactNode}) {
   return (
     <main className="p-3 container mx-auto h-full m-3 rounded-md bg-white">
       <header className="flex items-center gap-2 mb-8 flex-wrap">
-        <h1 className="text-4xl font-bold text-pink-600 flex-grow">SunDASH</h1>
+        <h1 className="text-4xl font-bold text-pink-600 flex-grow">
+          <Link to='/'>SunDASH</Link>
+        </h1>
         <nav className="flex items-center gap-2">
           <Link to="/apps" className={clsx(buttonCN.outline, buttonCN.normal)}>Apps</Link>
           <Link to="/appstore" className={clsx(buttonCN.outline, buttonCN.normal)}>App Store</Link>
