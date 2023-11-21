e533b151e1843a14df568fc525f90128a95e4825
Juan D. Jara <juanorigami@gmail.com>
2023-11-21 23:42:09 +0100

    ad back button to config page

--- app/routes/config.tsx
+++ app/routes/config.tsx
@@ -1,8 +1,11 @@
-import { useLoaderData } from "@remix-run/react"
+import { ArrowLeftIcon } from "@heroicons/react/20/solid"
+import { useLoaderData, useNavigate } from "@remix-run/react"
+import clsx from "clsx"
 import { useMemo } from "react"
 import LogDisplay from "~/components/LogDisplay"
 import Layout from "~/components/layout"
 import env from "~/lib/env.server"
+import { buttonCN } from "~/lib/styles"
 
 export async function loader() {
   return {
@@ -26,6 +29,7 @@ export async function loader() {
 }
 
 export default function Config() {
+  const navigate = useNavigate()
   const envVars = useLoaderData<typeof loader>()
   const envVarsTxt = useMemo(() => {
     return Object.entries(envVars).map(([key, line]) => [
@@ -37,9 +41,14 @@ export default function Config() {
   
   return (
     <Layout>
-      <div className="mb-8">
-        <h2 className="text-3xl font-bold mb-1">Configuration</h2>
-        <p className="text-xl">These are the variables used for configuring the app.</p>
+      <div className="md:flex items-start gap-2">
+        <button onClick={() => navigate(-1)} className={clsx('block w-min mb-2', buttonCN.normal, buttonCN.icon, buttonCN.transparent)}>
+          <ArrowLeftIcon className='w-5 h-5' />
+        </button>  
+        <div className="mb-8">
+          <h2 className="text-3xl font-bold mb-1">Configuration</h2>
+          <p className="text-xl">These are the variables used for configuring the app.</p>
+        </div>
       </div>
       <LogDisplay text={envVarsTxt} hideScrollToBottom />
       <p className="my-6">
