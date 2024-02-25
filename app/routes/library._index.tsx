44519917d2c65b1f8a4e67ba40d257c1cf3b3bb3
Juan D. Jara <juanorigami@gmail.com>
2024-02-25 21:29:34 +0100

    hide other apps when no content is available

--- app/routes/library._index.tsx
+++ app/routes/library._index.tsx
@@ -104,43 +104,47 @@ export default function Library() {
           </ul>
         </section>
       ))}
-      <h2 className="mt-12 mb-3">
-        <span className="text-3xl font-semibold">Other apps</span>
-        <span className="ml-2 text-sm text-gray-500">{runningProjects.length} projects</span>
-      </h2>
-      <p className="mb-6 max-w-prose">
-        This page lists all the projects that are currently running in Docker. These projects are not part of the app library but are running on the system.
-      </p>
-      {runningProjects.map((project) => (
-        <section key={project.key} className={clsx(
-          'mb-6 px-2 rounded-lg relative',
-          'shadow-none hover:bg-red-50/50 hover:shadow hover:shadow-pink-100 transition-all'
-        )}>
-          <Link to={`/library/${project.key}`} className="absolute inset-0">
-            <span className="sr-only">{project.key}</span>
-          </Link>
-          <h3 className="border-b mb-2 pt-1 pb-2 px-1.5">
-            <p className="text-xl font-bold"> {project.key} </p>
-            <p className="text-sm text-gray-500"> {project.folder} </p>
-          </h3>
-          <ul className="py-2 flex flex-wrap justify-start gap-4">
-            {project.ymlFiles.map(({ path, meta }) => {
-              const { State, Status } = containerServiceMap[meta?.serviceKey] || {}
-              return (
-                <AppCard
-                  key={`${project.key}?service=${meta?.serviceKey}`}
-                  link={`/library/${project.key}?service=${meta?.serviceKey}`}
-                  title={meta?.title || path}
-                  logo={meta?.logo || ''}
-                  state={State}
-                  status={Status}
-                  enabled={meta?.enabled}
-                />
-              )
-            })}
-          </ul>
-        </section>
-      ))}
+      {runningProjects.length > 0 && (
+        <>
+          <h2 className="mt-12 mb-3">
+            <span className="text-3xl font-semibold">Other apps</span>
+            <span className="ml-2 text-sm text-gray-500">{runningProjects.length} projects</span>
+          </h2>
+          <p className="mb-6 max-w-prose">
+            This page lists all the projects that are currently running in Docker. These projects are not part of the app library but are running on the system.
+          </p>
+          {runningProjects.map((project) => (
+            <section key={project.key} className={clsx(
+              'mb-6 px-2 rounded-lg relative',
+              'shadow-none hover:bg-red-50/50 hover:shadow hover:shadow-pink-100 transition-all'
+            )}>
+              <Link to={`/library/${project.key}`} className="absolute inset-0">
+                <span className="sr-only">{project.key}</span>
+              </Link>
+              <h3 className="border-b mb-2 pt-1 pb-2 px-1.5">
+                <p className="text-xl font-bold"> {project.key} </p>
+                <p className="text-sm text-gray-500"> {project.folder} </p>
+              </h3>
+              <ul className="py-2 flex flex-wrap justify-start gap-4">
+                {project.ymlFiles.map(({ path, meta }) => {
+                  const { State, Status } = containerServiceMap[meta?.serviceKey] || {}
+                  return (
+                    <AppCard
+                      key={`${project.key}?service=${meta?.serviceKey}`}
+                      link={`/library/${project.key}?service=${meta?.serviceKey}`}
+                      title={meta?.title || path}
+                      logo={meta?.logo || ''}
+                      state={State}
+                      status={Status}
+                      enabled={meta?.enabled}
+                    />
+                  )
+                })}
+              </ul>
+            </section>
+          ))}
+        </>
+      )}
       <div className="pb-12"></div>
     </Layout>
   )
